146fe9915be920e32dee6ebec44b2cb82399a4bb
vincerp <vincependrell@gmail.com>
2014-02-04 12:53:45 +0100

    Chained and Foldered

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float forwardSpeed = 1f;
+	
+	public float horizontalRotationSpeed = 1f;
+	public float verticalLaptopMovementSpeed = 1f;
+
+	
+	void Update () {
+	
+	}
+}
