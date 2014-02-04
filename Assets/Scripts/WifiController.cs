81e7e089d3a383875843991eba16fea66848b981
vincerp <vincependrell@gmail.com>
2014-02-04 15:44:28 +0100

    some additions

---
+++ Assets/Scripts/WifiController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WifiController : MonoBehaviour {
+	
+	public Transform player;
+	public WifiSignal activeSignal;
+	public List<WifiSignal> signals;
+
+	public float GetDistanceFromSignal(){
+		if(null == activeSignal || null == player){
+			Debug.LogError("Something is wrong!");
+			return;
+		}
+
+		Vector3 playerPos = player.position;
+		Vector3 signalPosition = activeSignal.transform.position;
+
+		float distance = Vector3.Distance(playerPos, signalPosition);
+
+		return distance;
+	}
+
+	public void ChangeActiveSignal(){
+		//change it here!
+		//random signal, not the last
+	}
+}
