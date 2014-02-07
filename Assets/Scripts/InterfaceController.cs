d7ef458ca9ecdf0fff61df5191257884830b1be2
mobra <zagni.m@gmail.com>
2014-02-07 16:40:13 +0100

    ge

---
+++ Assets/Scripts/InterfaceController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InterfaceController : MonoBehaviour {
+
+	
+	public TextMesh timerText;
+	public TextMesh filesText;
+
+	public Renderer wifiSymbol; //the wifi bar
+	public Transform downloadBar;
+
+	public void UpdateTimer(float timeRemaining){
+		timeRemaining = Mathf.Ceil(timeRemaining);
+
+		string txt = "00:"+timeRemaining;
+
+		timerText.text = txt;
+	}
+
+	public void UpdateWifiSymbol(float currentDistance, float maxDistance){
+		float wifiPercent = 1-Mathf.Clamp01(currentDistance/maxDistance);
+
+		//TODO: change shader here
+	}
+
+	public void UpdateDownloadBar(float currentUpload, float totalUpload){
+		float percent = currentUpload/totalUpload;
+
+		downloadBar.localScale = new Vector3(percent, 1f, 1f);
+	}
+
+	public void UploadFilesSent(int filesSent, int filesTotal){
+		filesText.text = ""+filesSent+"/"+filesTotal;
+	}
+}
