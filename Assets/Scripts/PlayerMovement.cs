da0f9508c0b04fc1b749af20da4e0d44fd54a6f5
mobra <zagni.m@gmail.com>
2014-02-07 16:36:25 +0100

    laptop movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
 	public float maxVel = 2f;
+	public float maxArmsH = 340;
+	public float minArmsH = 20;
 
-	
+	public Transform arms;
 	public float horizontalRotationSpeed = 1f;
 	public float verticalLaptopMovementSpeed = 1f;
 
@@ -19,6 +21,7 @@ public class PlayerMovement : MonoBehaviour {
 		rigidbody.AddRelativeForce(0, 0, 10);
 		rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxVel);
 
+	
 
 
 
@@ -30,19 +33,31 @@ public class PlayerMovement : MonoBehaviour {
 
 		if(ChainJam.GetButtonPressed(ChainJam.PLAYER.PLAYER1,ChainJam.BUTTON.LEFT))
 		{
-			rigidbody.MoveRotation(rigidbody.rotation*(Quaternion.Euler(Vector3.up*(-horizontalRotationSpeed))));
+			rigidbody.MoveRotation(rigidbody.rotation*(Quaternion.Euler(Vector3.up*-horizontalRotationSpeed)));
 		}
 
 		if(ChainJam.GetButtonPressed(ChainJam.PLAYER.PLAYER1,ChainJam.BUTTON.UP))
 		{
+			arms.Rotate(Vector3.right*-verticalLaptopMovementSpeed);
+			//if (arms.localEulerAngles.x > 340 || arms.localEulerAngles.x <= 0)
 
 		}
+
+
 
 		if(ChainJam.GetButtonPressed(ChainJam.PLAYER.PLAYER1,ChainJam.BUTTON.DOWN))
 		{
+			//if (arms.localEulerAngles.x )
+			arms.Rotate(Vector3.right*verticalLaptopMovementSpeed);
 
 		}
-		
+
+		if (arms.localEulerAngles.x < maxArmsH && arms.localEulerAngles.x > 180)
+			arms.localRotation = Quaternion.Euler(maxArmsH,0,0);
+
+		if (arms.localEulerAngles.x > minArmsH && arms.localEulerAngles.x < 180)
+		{arms.localRotation = Quaternion.Euler(minArmsH,0,0);}
+
 		if(ChainJam.GetButtonPressed(ChainJam.PLAYER.PLAYER1,ChainJam.BUTTON.A))
 		{
 
